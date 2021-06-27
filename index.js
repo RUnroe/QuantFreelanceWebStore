2dc0277e27ea0e62ebbffb4d5b83e3ee6bb3458e
RUnroe <ryanunroe@gmail.com>
2021-06-27 10:38:23 -0600

    Create folder structure and start pug layout

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const path = require('path');
 const session = require("express-session");
 
 const app = express();
+const snowmachine = new (require('snowflake-generator'))(1420070400000);
+
+app.listen(3000);
