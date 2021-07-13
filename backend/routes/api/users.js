d78562e20bd08d5aff3eeb548437c4bbec2f9532
RUnroe <ryanunroe@gmail.com>
2021-07-13 16:58:42 -0600

    Test auth method

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -24,9 +24,10 @@ const authenticate = (req, res, next) => {
 		.then(user_id => {
 			console.log(user_id, 'logged in');
 			if (user_id) {
-				req.session.user_id = user_id.toString();
+				req.session.user_id = user_id;
 				res.statusMessage = 'Authenticated';
 				res.status(204).end();
+				console.log(res);
 				return;
 			}
 			res.sendStatus(401);
