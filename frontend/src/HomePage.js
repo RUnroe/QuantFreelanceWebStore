d6b7f110f65fa466a3f6c552809f9f3aea226605
RUnroe <ryanunroe@gmail.com>
2021-07-13 15:54:36 -0600

    Test react fetch requests

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 export default function HomePage() {
+    const firstName = createRef();
+    const logIn = () => {
+        const data = {
+            identifier: "RUnroe",
+            password: "1234aaAb"
+        }
+        fetch("http://localhost:3005/api/auth", {
+            method: 'POST', 
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then( response => console.log(response));
+    }
+    const updateUser = () => {
+        const data = {first_name: firstName.current.value};
+        console.log(data);
+        fetch("http://localhost:3005/api/user", {
+            method: 'PUT', 
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then( response => console.log(response));
+    }
     return(
-        <h1>Home</h1>
+        <div className="section">
+            <h1>Home</h1>
+            <button onClick={logIn}>Log In</button>
+            <input type="text" ref={firstName} placeholder="First Name" />
+            <button onClick={updateUser}> Update User</button>
+        </div>
     );
 
 }
