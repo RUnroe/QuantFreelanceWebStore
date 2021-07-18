86e921db1bd4c2c15a2652ecdbd3cdad973c6f04
RUnroe <ryanunroe@gmail.com>
2021-07-18 13:54:20 -0600

    Change form setup

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { createRef, useState } from "react";
-import {ImageInput} from "./../partials/form";
+import {ImageInput, SignupForm} from "./../partials/form";
 
 export default function HomePage() {
     const firstName = createRef();
@@ -29,26 +29,14 @@ export default function HomePage() {
         }).then( response => console.log(response));
     }
 
-    const postIcon = event => {
-        event.preventDefault();
-        const formData = new FormData();
-        formData.append('icon', document.getElementById("iconInput").files[0]);
-        fetch('/api/icons', {
-            method: 'POST',
-            body: formData
-        }).then(response => response.json())
-        .then(data => console.log(data));
-    }
+    
     return(
         <div className="section">
             <h1>Home</h1>
             <button onClick={logIn}>Log In</button>
             <input type="text" ref={firstName} placeholder="First Name" />
             <button onClick={updateUser}> Update User</button>
-            <form id="form" method="POST" onSubmit={postIcon}>
-                <ImageInput />
-                <button type="submit">Submit</button>
-            </form>
+                <SignupForm />
         </div>
     );
 
--- frontend/src/partials/form.js
+++ frontend/src/partials/form.js
@@ -1,11 +1,34 @@
+import { createRef } from "react";
 
 
 function SignupForm() {
+    const image = createRef();
 
+    const postIcon = event => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('icon', image.current.files[0]);
+        fetch('/api/icons', {
+            method: 'POST',
+            body: formData
+        }).then(response => response.json())
+        .then(data => console.log(data));
+    }
+    return (
+        <form id="form" method="POST" onSubmit={postIcon}>
+            <div className="form-block">
+                <label className="input-label">Icon</label>
+                <label className="input-label file-input btn"> Upload Image
+                    <input id="imageInput" ref={image} type="file" name="newIcon" accept="image/png, image/jpeg, image/jpg, image/svg" />
+                </label>
+                <span id="imageErrorMsg" className="error-message hidden">Incorrect file type</span>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
 function ImageInput({labelName}) {
-
 
     return (
         <div className="form-block">
@@ -17,6 +40,9 @@ function ImageInput({labelName}) {
         </div>
     );
 }
+
+
+
 
 export {
     SignupForm, 
