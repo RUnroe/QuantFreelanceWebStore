61cad0337625b545a5beac0d7e214cba4fc0740f
RUnroe <ryanunroe@gmail.com>
2021-07-18 14:10:59 -0600

    Get file uploading package

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const session = require("express-session");
 const bodyParser = require('body-parser');
 const MongoStore = require('connect-mongo');
+const upload = require('multer')({ dest: __dirname + '/public/icons' });
 
 
 
