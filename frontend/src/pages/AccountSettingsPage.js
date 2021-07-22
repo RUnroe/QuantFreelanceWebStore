f527c9328a736acccbd0f96333d8cc85a8ed076f
RUnroe <ryanunroe@gmail.com>
2021-07-22 14:26:37 -0600

    Create temporary image upload page

--- frontend/src/pages/AccountSettingsPage.js
+++ frontend/src/pages/AccountSettingsPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ImageForm } from "../partials/form";
+
 export default function AccountSettingsPage() {
     return(
+        <>
         <h1>Account Settings</h1>
+        <ImageForm />
+        </>
     );
 
 }
