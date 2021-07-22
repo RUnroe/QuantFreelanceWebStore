b9876a660de9f9ff010c1732510ae90d3f54cfd3
RUnroe <ryanunroe@gmail.com>
2021-07-22 14:25:20 -0600

    Create temporary image upload page

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ImageForm } from "../partials/form";
+
 export default function SignupPage() {
     return(
+        <>
         <h1>Sign Up</h1>
+        <ImageForm />
+        </>
     );
 
 }
