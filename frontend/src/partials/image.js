9de9705461ef78fe88e0ef8a6ed3a588a92b65d9
RUnroe <ryanunroe@gmail.com>
2021-07-22 15:45:21 -0600

    Highlight selected image

--- frontend/src/partials/image.js
+++ frontend/src/partials/image.js
@@ -4,14 +4,15 @@ function ImageSelectModal() {
     const [image, setImage] = useState();
     const [selectedImageUrl, setSelectedImageUrl] = useState();
     const [modalState, setModalState] = useState("upload");
-    const [userImages, setUserImages] = useState([]);
+    const [userImages, setUserImages] = useState([]);/////////////////////////////
     const [userImageJSX, setUserImageJSX] = useState([]);
 
     const getUsersImages = async () => {
         return fetch("/api/icons/user").then(result => result.json()).then(data => {
             const images = data.map(item => item.url);
             console.log(images);
-            setUserImages(images);
+            setUserImages([...images]);
+            console.log(userImages);
             renderUserImagesJSX(images, -1);
             return images;
         });
@@ -53,7 +54,7 @@ function ImageSelectModal() {
             console.log(image.value);
         }
         else if (modalState === "select") {
-
+            console.log(selectedImageUrl);
         }
     }
     return (
