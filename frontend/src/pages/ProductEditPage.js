059ebaacc485800132025aba3328ef2643d7b584
RUnroe <ryanunroe@gmail.com>
2021-07-22 23:08:28 -0600

    Create convertHeader method

--- frontend/src/pages/ProductEditPage.js
+++ frontend/src/pages/ProductEditPage.js
@@ -1,15 +1,61 @@
 import React, { useEffect, useState } from "react";
 import "../styles/productPage.css";
 import {ImageSelectModal} from "../partials/image";
+
+const genId = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const convertHeader = (headerObject) => {
+    const props = headerObject.properties;
+    const jsx = [];
+    const style = {
+        fontWeight: props.fontWeight
+    }
+    if(props.style === "italics") style.fontStyle = "italics";
+    if(props.style === "underline") style.textDecoration = "underline";
+
+    switch(headerObject.properties.headerType) {
+        case "h1":
+            {{}}
+            jsx.push(<h1 style={style}>{props.value}</h1>);
+        break;
+        case "h2":
+            jsx.push(<h2 style={style}>{props.value}</h2>);
+        break;
+        case "h3":
+            jsx.push(<h3 style={style}>{props.value}</h3>);
+        break;
+        case "h4":
+            jsx.push(<h4 style={style}>{props.value}</h4>);
+        break;
+        case "h5":
+            jsx.push(<h5 style={style}>{props.value}</h5>);
+        break;
+        case "h6":
+            jsx.push(<h6 style={style}>{props.value}</h6>);
+        break;
+
+    }
+    return jsx;
+}
+
+
 export default function ProductEditPage() {
     const [imageSelectModalSetter, setImageSelectModalSetter] = useState();
-
+    
+    const [productId, setProductId] = useState(""); 
     const [title, setTitle] = useState("");
     const [coverImg, setCoverImg] = useState("");
     const [description, setDescription] = useState("");
     const [price, setPrice] = useState(0);
     const [category, setCategory] = useState("");
     const [pageStructure, setPageStructure] = useState([]);
+
+    const [pageStructureJSX, setPageStructureJSX] = useState([]);
 
     const [selectedElementId, setSelectedElementId] = useState();
 
@@ -20,11 +66,82 @@ export default function ProductEditPage() {
         setCoverImg("https://via.placeholder.com/135x65");
         setDescription("this is a description");
         setPrice("30.00");
-        setCategory("DesignArt");
+        setCategory("ProgrammingTech");
+        setPageStructure([
+            {
+                type: "header",
+                id:"87c650d6-a60c-47c4-a7b8-f7537ba9d603",
+                position:1,
+                properties: {
+                    value: "This is an example of a header",
+                    fontWeight: 400,
+                    headerType: "h1",
+                    style: "none" //(italics, underline, none)
+                }
+            },
+            {
+                type: "split",
+                id:"87c650d6-a60c-47c4-a7b8-f7537ba9d604",
+                position:2,
+                properties: {
+                    splitType: 1,
+                    children: [
+                        {
+                            type: "paragraph",
+                            id:"87c650d6-a60c-47c4-a7b8-f7537ba9d605",
+                            position:2,
+                            properties: {
+                                value: "This is an example of a nested paragrah",
+                                fontWeight: 400,
+                                style: "none", //(italics, underline, none)
+                                align: "center"
+                            }
+                        },
+                        {
+                            type: "image",
+                            id:"87c650d6-a60c-47c4-a7b8-f7537ba9d606",
+                            position:2,
+                            properties: {
+                                src: "https://via.placeholder.com/135x65",
+                                align: "right",
+                                width: "100%"
+                            }
+                        },
+                    ]
+                }
+            }
+
+        ]);
     }, []);
 
+
+    useEffect(() => {
+        convertPageStructureToJSX();
+    }, [pageStructure]);
+
+    const convertPageStructureToJSX = () => {
+        const tempJSX = [];
+        pageStructure.forEach(pageElement => {
+            switch(pageElement.type) {
+                case "header": 
+                    tempJSX.push(convertHeader(pageElement));
+                break;
+            }
+        });
+        setPageStructureJSX(tempJSX);
+    }
     const savePage = () => {
-        console.log(title, coverImg, description, price, category, pageStructure);
+        const pageData = {
+            title,
+            coverImage: coverImg,
+            description,
+            price,
+            category,
+            page_structure: pageStructure
+        }
+        console.log(pageData);
+        //post the pageData to the updateProduct method
+        console.log(genId());
     }
 
     const selectImage = (setter) => {
@@ -34,7 +151,7 @@ export default function ProductEditPage() {
         <>
         <div className="product-page">
             <div className="product-main container">
-                <h1>Sample Header</h1>
+                {/* <h1>Sample Header</h1>
                 <p>Sample paragraph</p>
                 <div className="image">
                     <img src="https://via.placeholder.com/600x250" alt="sample" />
@@ -60,10 +177,11 @@ export default function ProductEditPage() {
                     <div className="right-side">
                         <p>Right text</p>
                     </div>
-                </div>
+                </div>  */}
 
+                {pageStructureJSX}
 
-
+                {/* ADD MODAL */}
                 <div className="modal" id="addElementModal">
                     <div className="modal-header">
                         <h2>Add an Element:</h2>
