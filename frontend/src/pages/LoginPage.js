78b88f16fcefc94ae543e5b3f9ebc0877647e234
RUnroe <ryanunroe@gmail.com>
2021-07-27 11:14:04 -0600

    Add button styles to general css

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,9 +2,11 @@ import React from "react";
 import { LogInForm } from "../partials/form";
 export default function LoginPage({checkAuth}) {
     return(
-        <div className="section">
-            <h1>Log In</h1>
-            <LogInForm checkAuth={checkAuth} />
+        <div className="container">
+            <div className="section">
+                <h1>Log In</h1>
+                <LogInForm checkAuth={checkAuth} />
+            </div>
         </div>
     );
 
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -3,10 +3,18 @@ import { ImageForm } from "../partials/form";
 
 export default function SignupPage() {
     return(
-        <>
-        <h1>Sign Up</h1>
-        <ImageForm />
-        </>
+        <div className="container">
+            <div className="section">
+                <h1>Sign Up</h1>
+                <ImageForm />
+                <button className="btn blue">Primary</button>
+                <button className="btn blue-outline">Primary Outline</button>
+                <button className="btn green">Secondary</button>
+                <button className="btn green-outline">Secondary Outline</button>
+                <button className="btn danger">Danger</button>
+                <button className="btn danger-outline">Danger Outline</button>
+            </div>
+        </div>
     );
 
 }
