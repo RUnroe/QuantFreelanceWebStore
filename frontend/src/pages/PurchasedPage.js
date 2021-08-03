ab81d6e6476fc1f3ca5ab1126848b22718e7c61c
RUnroe <ryanunroe@gmail.com>
2021-08-03 16:31:48 -0600

    Create purchased page

--- frontend/src/pages/PurchasedPage.js
+++ frontend/src/pages/PurchasedPage.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Redirect } from 'react-router-dom';
+import { useParams, Redirect, Link } from 'react-router-dom';
 import "../styles/purchased.css";
-import ProductCard from "../partials/productCard";
 
 export default function PurchasePage() {
+    const {order_id} = useParams();
+    const [order, setOrder] = useState();
+    const [product, setProduct] = useState();
 
+    useEffect(() => {
+        fetch(`/api/order/${order_id}`, {credentials: "include"})
+        .then(response => response.json())
+        .then(orderData => {
+            setOrder(orderData);
+            fetch(`/api/product/${orderData.product_id}`, {credentials: "include"})
+            .then(response => response.json())
+            .then(productData => {
+                setProduct(productData);
+            });
+        });
+    }, []);
     return (
         <div className="section purchased">
             <div className="container">
-                <h1></h1>
+                <h1>Order Confirmation</h1>
+                <p>You have sucessfully purchased { product  && product.title ? product.title : "the product"}! Please wait for the seller to accept your order.</p>
+                
+
+                <Link class="btn blue center" to="/">Continue Browsing</Link>
             </div>
         </div>
 
